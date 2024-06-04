d18e354d190e33090fb439764e9037785c9f73d8
Preetam Joshi <info@aimon.ai>
2024-06-04 14:52:19 -0700

    Bumping up the version for the release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='aimon',
     python_requires='>3.8.0',
     packages=find_packages(),
-    version="0.1.0",
+    version="0.2.0",
     install_requires=[
         "requests"
     ],

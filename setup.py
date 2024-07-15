3f0677bd90d403f433fa7799162c9de9b1ebe5c3
Ramon Marques <ramon.marques@toptal.com>
2024-07-15 13:30:39 -0300

    Including release notes

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
+    changelog = fh.read()
+
 setup(
     name='aimon',
     python_requires='>3.8.0',
@@ -11,6 +17,6 @@ setup(
     author='AIMon',
     author_email='info@aimon.ai',
     description='The AIMon SDK that is used to interact with the AIMon API and the product.',
-    long_description=open('README.md').read(),
+    long_description=long_description + "\n\n" + changelog,
     long_description_content_type='text/markdown',
 )

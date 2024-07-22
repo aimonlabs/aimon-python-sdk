ce5b24079d238bce5f09dda01938f4ecfaca51b6
Ramon Marques <ramon.marques@toptal.com>
2024-07-22 16:18:35 -0300

    Fix readmes

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+# Read the content of your README file
+with open("README_PyPi.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='aimon',
     python_requires='>3.8.0',
@@ -14,6 +18,6 @@ setup(
     author='AIMon',
     author_email='info@aimon.ai',
     description='The AIMon SDK that is used to interact with the AIMon API and the product.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )

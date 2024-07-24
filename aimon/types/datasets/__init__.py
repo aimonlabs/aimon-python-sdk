b4b4de39c76849a82d3125883c722e6bd33068c5
Ramon Celestino <ramoncelesteramos@gmail.com>
2024-07-24 12:28:58 -0300

    Include dataset collection retrieve (#18)
    Co-authored-by: Ramon Marques <ramon.marques@toptal.com>

--- aimon/types/datasets/__init__.py
+++ aimon/types/datasets/__init__.py
@@ -6,3 +6,5 @@ from .record_list_params import RecordListParams as RecordListParams
 from .record_list_response import RecordListResponse as RecordListResponse
 from .collection_create_params import CollectionCreateParams as CollectionCreateParams
 from .collection_create_response import CollectionCreateResponse as CollectionCreateResponse
+from .collection_retrieve_params import CollectionRetrieveParams as CollectionRetrieveParams
+from .collection_retrieve_response import CollectionRetrieveResponse as CollectionRetrieveResponse
---
+++ aimon/types/datasets/collection_retrieve_response.py
@@ -0,0 +1,27 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Optional
+from datetime import datetime
+
+from ..._models import BaseModel
+
+__all__ = ["CollectionRetrieveResponse"]
+
+
+class CollectionRetrieveResponse(BaseModel):
+    dataset_ids: List[str]
+    """A list of dataset IDs included in this collection."""
+
+    name: str
+
+    id: Optional[str] = None
+
+    company_id: Optional[str] = None
+
+    creation_time: Optional[datetime] = None
+
+    description: Optional[str] = None
+
+    last_updated_time: Optional[datetime] = None
+
+    user_id: Optional[str] = None

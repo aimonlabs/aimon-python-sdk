562119fcf0cd099906b6276e53ada68b31867273
Preetam Joshi <info@aimon.ai>
2024-08-18 10:59:00 -0700

    Fixing bug in analyze_prod decorator to set the application stage to prod

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='aimon',
     python_requires='>3.8.0',
     packages=find_packages(),
-    version="0.7.0",
+    version="0.7.1",
     install_requires=[
         "httpx",
         "distro",

7e6a47ffa35bfd8ef58af1fdcc4fb742ea99deb1
Devvrat Bhardwaj <bhardwaj.devvrat@gmail.com>
2025-11-26 14:18:26 -0500

    Updated CLIENT_BASE_URL + Version Bump to 0.12.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='aimon',
     python_requires='>3.8.0',
     packages=find_packages(),
-    version="0.12.2",
+    version="0.12.3",
     install_requires=[
         "annotated-types~=0.6.0",
         "anyio~=4.9.0",
